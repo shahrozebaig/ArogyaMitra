3b7aa097a3d67ffa2b018d7090ef01816aa1fa42
Shahroze Baig <150318142+shahrozebaig@users.noreply.github.com>
2026-04-30 15:34:53 +0530

    Update nutrition_model.py

--- backend/models/nutrition_model.py
+++ backend/models/nutrition_model.py
@@ -1,18 +1,11 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
-
 from database import Base
-
-
 class NutritionPlan(Base):
     __tablename__ = "nutrition_plans"
-
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
-
     calories = Column(Integer)
     diet_type = Column(String)
-
     plan_json = Column(Text)
-
     user = relationship("User")
