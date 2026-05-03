892adf915c55a51ad3f9ade55af72146bc985470
Shahroze Baig <shahrozeb98@gmail.com>
2026-05-03 12:18:10 +0530

    Updated

--- backend/schemas/health_schema.py
+++ backend/schemas/health_schema.py
@@ -1,27 +1,30 @@
 from pydantic import BaseModel
 class HealthCreate(BaseModel):
-    age: int
-    height: int
-    weight: int
-    fitness_goal: str
-    fitness_level: str
-    workout_location: str
-    workout_time: str
-    dietary_preference: str = "Vegetarian"
+    age: int | None = 0
+    height: float | None = 0.0
+    weight: float | None = 0.0
+    gender: str | None = "Male"
+    fitness_goal: str | None = "Stay Fit"
+    fitness_level: str | None = "Beginner"
+    workout_location: str | None = "Home"
+    workout_time: str | None = "Morning"
+    dietary_preference: str | None = "Vegetarian"
     allergies: str | None = None
     medical_conditions: str | None = None
+
 class HealthResponse(BaseModel):
     id: int
     user_id: int
-    age: int
-    height: int
-    weight: int
-    fitness_goal: str
-    fitness_level: str
-    workout_location: str
-    workout_time: str
+    age: int | None = 0
+    height: float | None = 0.0
+    weight: float | None = 0.0
+    gender: str | None = "Male"
+    fitness_goal: str | None = "Stay Fit"
+    fitness_level: str | None = "Beginner"
+    workout_location: str | None = "Home"
+    workout_time: str | None = "Morning"
     dietary_preference: str | None = "Vegetarian"
     allergies: str | None = None
     medical_conditions: str | None = None
     class Config:
-        from_attributes = True
+        from_attributes = True
