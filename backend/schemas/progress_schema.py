e881b1e50329e1fa140114662f1816bb2ea9d5f1
Shahroze Baig <shahrozeb98@gmail.com>
2026-05-04 22:45:08 +0530

    Modified

--- backend/schemas/progress_schema.py
+++ backend/schemas/progress_schema.py
@@ -5,6 +5,8 @@ class ProgressCreate(BaseModel):
     calories_burned: float
     workout_completed: int
     healthy_meals_count: int | None = 0
+    status: str | None = "Completed"
+    workout_id: int | None = None
 class ProgressResponse(BaseModel):
     id: int
     user_id: int
@@ -12,6 +14,8 @@ class ProgressResponse(BaseModel):
     calories_burned: float
     workout_completed: int
     healthy_meals_count: int
+    status: str
+    workout_id: int | None = None
     created_at: datetime
     class Config:
         from_attributes = True
