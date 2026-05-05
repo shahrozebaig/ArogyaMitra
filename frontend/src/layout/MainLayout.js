03192bfbef43dddaf90ef9cb392ae22ebd96e116
Shahroze Baig <shahrozeb98@gmail.com>
2026-05-05 13:46:26 +0530

    Final Update

--- frontend/src/layout/MainLayout.js
+++ frontend/src/layout/MainLayout.js
@@ -1,16 +1,11 @@
 import Navbar from "../components/Navbar";
 function MainLayout({ children }) {
   return (
-    <div className="flex bg-[#0f111a] text-white min-h-screen font-['Inter']">
-      <div className="flex-1 flex flex-col h-screen overflow-hidden">
-        <Navbar />
-        <main className="flex-1 overflow-y-auto custom-scrollbar">
-          <div className="container mx-auto p-4 md:p-8">
-            {children}
-          </div>
-        </main>
-      </div>
-
+    <div style={{ background: "#f0fdf4", minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+      <Navbar />
+      <main style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        {children}
+      </main>
     </div>
   );
 }
