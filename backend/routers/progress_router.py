4a3eac68bc311133ab3fe66c6e08f30e1e99bcff
shahrozebaig <shahrozeb98@gmail.com>
2026-05-08 16:14:53 +0530

    DB Changed

--- backend/routers/progress_router.py
+++ backend/routers/progress_router.py
@@ -1,20 +1,18 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
 from database import get_db
 from schemas.progress_schema import ProgressCreate, ProgressResponse
 from services.progress_service import create_progress, get_progress_stats
+from utils.jwt_handler import get_current_user_id
 router = APIRouter()
 @router.post("/update", response_model=ProgressResponse)
-def update_progress(data: ProgressCreate, db: Session = Depends(get_db)):
-    user_id = 1
-    progress = create_progress(
+async def update_progress(data: ProgressCreate, db = Depends(get_db), user_id: str = Depends(get_current_user_id)):
+    progress = await create_progress(
         db,
         user_id,
         data.dict()
     )
     return progress
 @router.get("/stats")
-def get_stats(db: Session = Depends(get_db)):
-    user_id = 1
-    records = get_progress_stats(db, user_id)
+async def get_stats(db = Depends(get_db), user_id: str = Depends(get_current_user_id)):
+    records = await get_progress_stats(db, user_id)
     return records
