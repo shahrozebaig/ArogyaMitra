657241687a4a61d136773f3aa7da2ca854acde5a
shahrozebaig <shahrozeb98@gmail.com>
2026-05-08 16:21:13 +0530

    DB Changed

--- backend/utils/jwt_handler.py
+++ backend/utils/jwt_handler.py
@@ -1,6 +1,9 @@
-from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from config import JWT_SECRET_KEY, JWT_ALGORITHM, JWT_EXPIRATION_MINUTES
+from datetime import datetime, timedelta
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=JWT_EXPIRATION_MINUTES)
@@ -12,3 +15,8 @@ def verify_access_token(token: str):
         return payload
     except JWTError:
         return None
+def get_current_user_id(token: str = Depends(oauth2_scheme)):
+    payload = verify_access_token(token)
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return payload.get("user_id")
