316368ec28eb1c537668b323afe43d83044d672d
shahrozebaig <shahrozeb98@gmail.com>
2026-05-08 16:19:55 +0530

    DB Changed

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -1,25 +1,25 @@
-from sqlalchemy.orm import Session
-from models.user_model import User
 from utils.password_handler import hash_password, verify_password
 from utils.jwt_handler import create_access_token
-def register_user(db: Session, name: str, email: str, password: str):
-    existing_user = db.query(User).filter(User.email == email).first()
+async def register_user(db, name: str, email: str, password: str):
+    existing_user = await db.users.find_one({"email": email})
     if existing_user:
         return None
-    new_user = User(
-        name=name,
-        email=email,
-        password=hash_password(password)
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    new_user = {
+        "name": name,
+        "email": email,
+        "password": hash_password(password)
+    }
+    result = await db.users.insert_one(new_user)
+    new_user["id"] = str(result.inserted_id)
+    if "_id" in new_user: new_user.pop("_id")
     return new_user
-def login_user(db: Session, email: str, password: str):
-    user = db.query(User).filter(User.email == email).first()
+from fastapi import HTTPException
+async def login_user(db, email: str, password: str):
+    user = await db.users.find_one({"email": email})
     if not user:
-        return None
-    if not verify_password(password, user.password):
-        return None
-    token = create_access_token({"user_id": user.id})
+        raise HTTPException(status_code=404, detail="Account not found")
+    if not verify_password(password, user["password"]):
+        raise HTTPException(status_code=401, detail="Invalid password")
+    user["id"] = str(user.pop("_id"))
+    token = create_access_token({"user_id": user["id"]})
     return {"user": user, "token": token}
