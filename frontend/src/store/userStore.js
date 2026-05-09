3feac67ea8c27abce49d6a91e2c977b947aeecc6
shahrozebaig <shahrozeb98@gmail.com>
2026-05-09 10:46:03 +0530

    Modification

--- frontend/src/store/userStore.js
+++ frontend/src/store/userStore.js
@@ -7,16 +7,26 @@ const useUserStore = create((set) => ({
   setUser: (user, token) => {
     localStorage.setItem("user", JSON.stringify(user));
     localStorage.setItem("token", token);
-    if (user.profile_image) {
-      localStorage.setItem("arogya_profile_image", user.profile_image);
-      set({ user, token, profileImage: user.profile_image });
+    const profileImage = user.profile_image || null;
+    if (profileImage) {
+      localStorage.setItem("arogya_profile_image", profileImage);
     } else {
-      set({ user, token });
+      localStorage.removeItem("arogya_profile_image");
     }
+    set({ user, token, profileImage });
   },
   setProfileImage: (img) => {
     localStorage.setItem("arogya_profile_image", img);
-    set({ profileImage: img });
+    set((state) => ({
+      profileImage: img,
+      user: state.user ? { ...state.user, profile_image: img } : null
+    }));
+    // Also update the user object in localStorage if it exists
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser) {
+      storedUser.profile_image = img;
+      localStorage.setItem("user", JSON.stringify(storedUser));
+    }
   },
   setPreferredVoice: (voice) => {
     localStorage.setItem("arogya_preferred_voice", voice);
